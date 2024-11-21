1aaa29bd7bb106cc625c14c00ae54365c033daa6
akash_dynasty <akash@dynastygm.com>
2024-11-22 01:35:44 +0530

    [ Angular NgRx project added]

--- README.md
+++
@@ -1,2 +0,0 @@
-# ngrx-weather
-An example of how to use Ngrx on a simple Angular app
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ZipcodeEntryComponent } from './zipcode-entry/zipcode-entry.component';
-import {LocationService} from "./location.service";
-import { ForecastsListComponent } from './forecasts-list/forecasts-list.component';
-import {WeatherService} from "./weather.service";
-import { CurrentConditionsComponent } from './current-conditions/current-conditions.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import {RouterModule} from "@angular/router";
-import {routing} from "./app.routing";
-import {HttpClientModule} from "@angular/common/http";
+import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { CounterControlsComponent } from './counter-controls/counter-controls.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ZipcodeEntryComponent,
-    ForecastsListComponent,
-    CurrentConditionsComponent,
-    MainPageComponent
+    CounterOutputComponent,
+    CounterControlsComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    RouterModule,
-    routing
-  ],
-  providers: [LocationService, WeatherService],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
