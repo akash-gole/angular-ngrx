5eec4453733b118233698f288958ec798be3f139
akash_dynasty <akash@dynastygm.com>
2024-12-04 18:52:30 +0530

    Counter application using Ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { CounterOutputComponent } from './counter-output/counter-output.component';
 import { CounterControlsComponent } from './counter-controls/counter-controls.component';
 import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './store/conter.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { CounterEffects } from './store/conter.effects';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,15 @@ import { StoreModule } from '@ngrx/store';
     CounterOutputComponent,
     CounterControlsComponent,
   ],
-  imports: [BrowserModule, StoreModule.forRoot({}, {})],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({
+      counter: counterReducer,
+    }),
+    EffectsModule.forRoot([
+      CounterEffects
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
